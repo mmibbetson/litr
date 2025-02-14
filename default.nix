feb19aeef9c61e140e26c7dbbe86f4efa58fa54e
Matthew Mark Ibbetson <mmibbetson@gmail.com>
2025-02-14 15:27:07 +0200

    chore: version and binary name mgmt in default.nix

--- default.nix
+++ default.nix
@@ -13,12 +13,12 @@
 }:
 
 rustPlatform.buildRustPackage rec {
-  pname = "dn";
-  version = "0.1.3";
+  pname = "litr";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "mmibbetson";
-    repo = "dn";
+    repo = "litr";
     rev = "v${version}";
     sha256 = "sha256-D7ccY8Vq8grUwjojPzVlpBPmyU1iUwt41gYAs4rOzaI=";
   };
@@ -38,19 +38,19 @@ rustPlatform.buildRustPackage rec {
 
   postInstall =
     ''
-      installManPage man/dn.1 man/dn-new.1 man/dn-rename.1
+      installManPage man/litr.1 man/litr-new.1 man/litr-rename.1
       installShellCompletion \
-        --bash completions/dn.bash \
-        --fish completions/dn.fish \
-        --zsh completions/_dn
+        --bash completions/litr.bash \
+        --fish completions/litr.fish \
+        --zsh completions/_litr
     '';
 
   meta = with lib; {
     description = "A simple, minimal, and flexible command line utility for organising plaintext files.";
-    homepage = "https://mmibbetson.github.io/software/dn";
-    changelog = "https://github.com/mmibbetson/dn/CHANGELOG.md";
+    homepage = "https://mmibbetson.github.io/software/litr";
+    changelog = "https://github.com/mmibbetson/litr/CHANGELOG.md";
     license = licenses.gpl3Plus;
-    mainProgram = "dn";
+    mainProgram = "litr";
     maintainers = with maintainers; [
       mmibbetson
     ];
